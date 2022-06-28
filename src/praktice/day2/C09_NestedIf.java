beeaa889237458937fb5699e79c865f841a47309
Mehmet Bedrettin Aktas <mehmetbedrettinaktas@gmail.com>
2022-06-28 20:54:12 +0200

    "28.06.2022"

---
+++ src/praktice/day2/C09_NestedIf.java
@@ -0,0 +1,16 @@
+package praktice.day2;
+
+public class C09_NestedIf {
+    public static void main(String[] args) {
+          /*
+    Kullanici dan yas bilgisini alarak
+	    oy kullanma yasi :
+	 	age >= 18  ==> oy kullanmaya uygun
+	 	age >= 70  ==> uc kez oy kullanabilir
+	 	70 > age >=50 ==> iki kez oy kullanabilir
+	 	50 > age >=18 ==> bir kez oy kullanabilir, yazdiriniz
+	 */
+
+
+    }
+}
