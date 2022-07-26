477b8e14485b8f89691c441ad7068fbbe65f08bb
Mehmet Bedrettin Aktas <mehmetbedrettinaktas@gmail.com>
2022-07-26 22:09:27 +0200

    "26.07.2022"

---
+++ src/day32_StringBulder/C07_Delete.java
@@ -0,0 +1,22 @@
+package day32_StringBulder;
+
+public class C07_Delete {
+    public static void main(String[] args) {
+
+        StringBuilder sb = new StringBuilder("Java her zaman guzeldir");
+
+        System.out.println("sb.delete(8,9) = " + sb.delete(8, 9)); // Java herzaman guzeldir
+
+        System.out.println("sb.deleteCharAt(7) = " + sb.deleteCharAt(7)); // Java hezaman guzeldir
+
+        // basta baslayarak her loop'da ilk harfi silip kalani yazdiralim
+
+        int son =sb.length();
+        for (int i = 0; i <son; i++) {
+            sb.deleteCharAt(0);
+            System.out.println("sb = " + sb);
+
+      }
+
+    }
+}
